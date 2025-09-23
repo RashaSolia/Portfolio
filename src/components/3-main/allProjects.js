c5c9f32b4220790357cd090f10e7af9759045fe6
RashaSolia <rashasolia@gmail.com>
2025-09-23 19:25:53 +0300

    first commit

---
+++ src/components/3-main/allProjects.js
@@ -0,0 +1,92 @@
+export const allProjects = [
+  // Angular
+  {
+    name: "Musafer",
+    category: ["Angular"],
+    p: "A full-featured e-commerce web app built with Angular. It offers a modern UI, product management, and a seamless shopping experience.",
+    imgPath: "/images/Musafer.png",
+    linkGithub: "https://github.com/RashaSolia/MusaferProject",
+  },
+  // Flutter
+  {
+    name: "Flutter E-commerce",
+    category: ["Flutter"],
+    p: "A cross-platform e-commerce mobile app built with Flutter. Features include product browsing, cart, and a smooth user experience.",
+    imgPath: "/images/fluttercommerce.jpg",
+    linkGithub: "https://github.com/RashaSolia/Ecommerce_flutter",
+  },
+   {
+    name: "MarketlyApp",
+    category: ["React / React Native"],
+    p: "Marketly App is a web platform that connects local supermarkets with delivery agents, ensuring efficient product distribution, transparent pricing, and reliable area coverage.",
+    imgPath: "./../../../public/images/Screenshot 2025-09-23 190401.png",
+    linkGithub: "https://github.com/manoo25/MarketlyApp",
+
+  },
+
+   {
+    name: "Marketly Website",
+    category: ["React / React Native"],
+    p: "Marketly Dashboard is a web-based admin panel that enables efficient management of delivery agents, supermarkets, orders, and product distribution insights.",
+    imgPath: "./../../../public/images/Screenshot 2025-09-23 191944.png",
+    linkGithub: "https://github.com/manoo25/Marketly",
+  },
+  
+  // JavaScript
+  {
+    name: "Exclusive-E-commerce ",
+    category: ["JavaScript"],
+    p: "A trending products web app built with pure JavaScript. Discover and filter popular products with a fast and interactive UI.",
+    imgPath: "/images/Screenshot 2025-09-23 185116.png",
+    linkGithub: "https://github.com/Mohamed-Elfar/E-commerce-Website-Pure-JS",
+  },
+ 
+  {
+    name: "Facebook-MUI",
+    category: ["JavaScript"],
+    p: "A Facebook UI clone built with pure JavaScript. Experience a familiar social interface and interactions.",
+    imgPath: "/images/facebook.png",
+    linkGithub: "https://github.com/RashaSolia/PWADay2",
+  },
+   
+  {
+    name: "Book Marker App",
+    category: ["JavaScript"],
+    p: "A bookmarking app built with pure JavaScript. Save and organize your favorite links easily and efficiently.",
+    imgPath: "/images/bookmarkerpurejs.png",
+    linkGithub: "https://github.com/RashaSolia/Bookmarker",
+  },
+
+  {
+    name: "Yummy App",
+    category: ["JavaScript"],
+    p: "Yummy App is a mobile application that provides users with diverse food recipes, making it easy to discover, cook, and enjoy delicious meals.",
+    imgPath: "../../../public/images/Screenshot 2025-09-23 190903.png",
+    linkGithub: "https://github.com/RashaSolia/yummy",
+  },
+
+    {
+    name: "Quiz App",
+    category: ["JavaScript"],
+    p: "Quiz App is an interactive application that allows users to test their knowledge through engaging quizzes with instant feedback and score tracking.",
+    imgPath: "../../../public/images/Screenshot 2025-09-23 191431.png",
+    linkGithub: "https://github.com/RashaSolia/Quiz_App",
+  },
+ 
+  // AI & Simulation
+  // {
+  //   name: "genAi app (gpt-prompt-box)",
+  //   category: ["AI & Simulation"],
+  //   p: "A generative AI prompt box simulation UI for Telegram, built with JS. Interact with AI in a modern chat-like interface.",
+  //   imgPath: "/images/gpt-prompt-box.png",
+  //   linkGithub: "https://github.com/ahmedgaafer1/gpt-prompt-box",https://github.com/manoo25/MarketlyApp
+  // },
+ 
+  {
+    name: "TasksHub-ReactNative",
+    category: ["React / React Native"],
+    p: "A simple and intuitive to-do list mobile app using React Native. Organize your daily tasks efficiently with a user-friendly design.",
+    imgPath: "/images/TodoReactnative.png",
+    linkGithub: "https://github.com/RashaSolia/reactNativeTask4",
+  },
+];
